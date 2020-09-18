36e0939afe6e6ff3c7526f5bbb488b708b1ba957
Choi Wai Chung <wcchoi@outlook.com>
2020-09-18 23:36:57 +0800

    Copy selection to clipboard automatically

--- static/app.js
+++ static/app.js
@@ -1925,3 +1925,29 @@ var Keyboard = (function (Terminal, Analyzer) {
 })(Terminal, Analyzer)
 Keyboard.initialize(document.getElementById('container'))
 
+// REF: https://stackoverflow.com/a/30810322
+function copyTextToClipboard(text) {
+    var textArea = document.createElement("textarea");
+    textArea.value = text;
+
+    // Avoid scrolling to bottom
+    textArea.style.top = "0";
+    textArea.style.left = "0";
+    textArea.style.position = "fixed";
+
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+
+    try {
+        var successful = document.execCommand('copy');
+        var msg = successful ? 'successful' : 'unsuccessful';
+    } catch (err) {
+        console.error('Fallback: Oops, unable to copy', err);
+    }
+
+    document.body.removeChild(textArea);
+}
+
+// Copy selection to clipboard automatically
+term.onSelectionChange(function(ev) { copyTextToClipboard(term.getSelection()) })
