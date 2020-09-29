wcchoi/swell.sh

commit eef2328dc49e59c3433effaaea1bc01993a6265c

Improve swiping for curosr key

static/app.js
@@ -749,7 +749,7 @@ var Keyboard = (function (Terminal, Analyzer) {
         swipe: {onEnter: nullfn, onMove: nullfn, onUp: nullfn},
         longpress: {onEnter: nullfn, onMove: nullfn, onUp: nullfn } }
 
-    var cursorKey = {keyid: 'cursor', keytext: '✥', w:0.1, h: 0.25,
+    var cursorKey = {keyid: 'cursor', keytext: '✥', w:0.1, h: 0.25, bw: 0.05, bh: 0.125,
         click: Terminal.moveCursorLeft,
         timeout: 500 /*ms*/,
         swipe: {onEnter: CursorAction.start, onMove: CursorAction.move, onUp: CursorAction.up },
@@ -1676,10 +1676,14 @@ var Keyboard = (function (Terminal, Analyzer) {
     }
 
     var isPtWithinKey = function (pt, key) {
-        return pt.x >= key.cx-key.w/2
-            && pt.x <= key.cx+key.w/2
-            && pt.y >= key.cy-key.h/2
-            && pt.y <= key.cy+key.h/2
+        // bw, bh (boundary width/height) is used by cursor key to allow entering
+        // the swipe state with less distance
+        var dw = key.bw ? key.bw/2 : key.w/2
+        var dh = key.bh ? key.bh/2 : key.h/2
+        return pt.x >= key.cx-dw
+            && pt.x <= key.cx+dw
+            && pt.y >= key.cy-dh
+            && pt.y <= key.cy+dh
     }
 
     var getKeyUnderPoint = function (pt) {

commit 8bdb1e04443d33ec414873118af911c340434759

Fix cursor key hit box
Bug introduced in eef2328dc49e59c3433effaaea1bc01993a6265 where
it causes hit box for normal tap on cursor key smaller

static/app.js
@@ -1675,11 +1675,11 @@ var Keyboard = (function (Terminal, Analyzer) {
         _ctx.clearRect(0,0,_canvas.width, _canvas.height)
     }
 
-    var isPtWithinKey = function (pt, key) {
+    var isPtWithinKey = function (pt, key, isSwipe) {
         // bw, bh (boundary width/height) is used by cursor key to allow entering
         // the swipe state with less distance
-        var dw = key.bw ? key.bw/2 : key.w/2
-        var dh = key.bh ? key.bh/2 : key.h/2
+        var dw = isSwipe && key.bw ? key.bw/2 : key.w/2
+        var dh = isSwipe && key.bh ? key.bh/2 : key.h/2
         return pt.x >= key.cx-dw
             && pt.x <= key.cx+dw
             && pt.y >= key.cy-dh
@@ -1689,7 +1689,7 @@ var Keyboard = (function (Terminal, Analyzer) {
     var getKeyUnderPoint = function (pt) {
         for(var i = 0; i < _layoutsConfig[_currMode][_currLayout].length; i++) {
             var key = _layoutsConfig[_currMode][_currLayout][i]
-            if(isPtWithinKey(pt, key)) {
+            if(isPtWithinKey(pt, key, false)) {
                 return key
             }
         }
@@ -1828,7 +1828,7 @@ var Keyboard = (function (Terminal, Analyzer) {
 
             if(state === INITIAL_STATE) {
                 // INITIAL STATE mouse move out of downState key
-                if(!isPtWithinKey(currPt, downState.key)) {
+                if(!isPtWithinKey(currPt, downState.key, true)) {
                     window.clearTimeout(timeoutEvent)
                     currHighlight.style.fill = '#f0f0f0'  //dehighlight
 